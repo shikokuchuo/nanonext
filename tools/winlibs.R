91efe63c627f5a4bfc64624683102fa38589c20a
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2022-01-23 17:19:24 +0000

    configure tweaks for MacOS/Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,9 @@
 # nanonext - Download precompiled libnng for Windows ---------------------------
 
-if (!file.exists("../windows/nng-latest/include/nng.h")) {
+if (!file.exists("../windows/rwinlib-1.5.2/include/nng.h")) {
 
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/shikokuchuo/rwinlib/raw/main/nng-latest.zip", destfile = "nng-latest.zip", quiet = TRUE)
+  download.file("https://github.com/shikokuchuo/rwinlib/archive/refs/tags/v1.5.2.zip", destfile = "nng-latest.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("nng-latest.zip", exdir = "../windows")
   unlink("nng-latest.zip")
