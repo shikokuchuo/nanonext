bd1081a7d9b6df398759af9ff3d4b038b9532e49
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-11-29 14:14:30 +0000

    docs and tests

--- R/utils.R
+++ R/utils.R
@@ -167,7 +167,8 @@ parse_url <- function(url) .Call(rnng_url_parse, url)
 #' \sQuote{recvAio}).
 #'
 #' Is the object an object inheriting from class \sQuote{nano} i.e. a
-#' nanoSocket, nanoContext, nanoStream, nanoListener, nanoDialer or nano Object.
+#' nanoSocket, nanoContext, nanoStream, nanoListener, nanoDialer, nanoMonitor or
+#' nano Object.
 #'
 #' Is the object an ncurlSession (object of class \sQuote{ncurlSession}).
 #'
